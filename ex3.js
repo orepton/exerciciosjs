7a27adb9e3bdbe60e64c066ba45e65e37fe28b2b
zReptonL <caeoli39@gmail.com>
2024-05-03 16:48:10 -0300

    feat:Exercicio 03

---
+++ ex3.js
@@ -0,0 +1,13 @@
+let num = prompt("Digite um número:")
+
+if (num % 2 == 0) {
+    alert("O número é par!")
+}
+
+else if (num % 2 == 1) {
+    alert("O número é ímpar!")
+}
+
+else {
+    alert("O valor informado não é um número!")
+}
